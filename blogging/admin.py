217b843aa187ffbade465b91e226e3352aa07a09
alexfrain <47937169+alexfrain@users.noreply.github.com>
2021-11-15 18:53:59 -0800

    Implemented assignment07 requirements - reversing the relationship so you can only add categories to posts.

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+
+class PostsInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostsInline, ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
